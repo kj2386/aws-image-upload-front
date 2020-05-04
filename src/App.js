4532039b6d716eaf3dc33411d2c878c9ee1425ef
kj2386 <kj2386@gmail.com>
2020-05-04 13:33:27 -0600

    added post

--- src/App.js
+++ src/App.js
@@ -20,18 +20,36 @@ const UserProfiles = () => {
   return userProfiles.map((userProfile, index) => {
     return (
       <div key={index}>
-        <Dropzone />
+      <br />
+      <br/>
         <h1>{userProfile.username}</h1>
         <p>{userProfile.userProfileId}</p>
+        <Dropzone {...userProfile} />
+        <br />
       </div>
     );
   });
 };
 
-function Dropzone() {
+function Dropzone({ userProfileId }) {
   const onDrop = useCallback(acceptedFiles => {
     const file = acceptedFiles[0];
     console.log(file);
+    const formData = new FormData();
+    formData.append('file', file);
+
+    axios.post(`http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload`, 
+    formData,
+    {
+      headers: {
+        'Content-Type': 'mulitpart/form-data'
+      } 
+    }).then(() => {
+      console.log('file uploaded succesfully')
+    }).catch(err => {
+      console.log(err);
+    } );
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
@@ -39,9 +57,9 @@ function Dropzone() {
     <div {...getRootProps()}>
       <input {...getInputProps()} />
       {isDragActive ? (
-        <p>Drop the files here ...</p>
+        <p>Drop the image here ...</p>
       ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
+        <p>Drag 'n' drop profile image, or click to select profile image</p>
       )}
     </div>
   );
